6279be5cb45ae9ef719975eb4a4313f4761bcba0
GauravTarale77 <gauravtarale6@gmail.com>
2026-08-23 19:46:41 +0530

    Update url

--- src/data/founders.ts
+++ src/data/founders.ts
@@ -5,7 +5,7 @@ export type Founder = {
 };
 
 export const founders: Founder[] = [
-  { name: "Gaurav Tarale", photoUrl: "/founders/Gaurav.PNG", portfolioUrl: "https://portfolio-gaurav-tarale.vercel.app/" },
+  { name: "Gaurav Tarale", photoUrl: "/founders/Gaurav.png", portfolioUrl: "https://portfolio-gaurav-tarale.vercel.app/" },
   { name: "Tushar Chapre", photoUrl: "/founders/Tushar.PNG", portfolioUrl: "https://vercel.com" },
   { name: "Mansi Thakare", photoUrl: "/founders/Mansi.jpeg", portfolioUrl: "https://mansi-portfolioindexhtml.vercel.app/" },
   { name: "Shubham Urkude", photoUrl: "/founders/Me.jpeg", portfolioUrl: "https://vercel.com" },
