3cda6c9dcb2082f3eb9e3177b30caff8d33274a7
Gaurav Tarale <gauravtarale6@gmail.com>
2026-09-11 17:57:58 +0530

    Photo

--- src/data/founders.ts
+++ src/data/founders.ts
@@ -8,5 +8,5 @@ export const founders: Founder[] = [
   { name: "Gaurav Tarale", photoUrl: "/founders/Gaurav.png", portfolioUrl: "https://portfolio-gaurav-tarale.vercel.app/" },
   { name: "Tushar Chapre", photoUrl: "/founders/Tushar.PNG", portfolioUrl: "/coming-soon" },
   { name: "Mansi Thakare", photoUrl: "/founders/Mansi.jpeg", portfolioUrl: "https://mansi-portfolioindexhtml.vercel.app/" },
-  { name: "Shubham Urkude", photoUrl: "/founders/Me.jpeg", portfolioUrl: "/coming-soon" },
+  { name: "Apoorva Urkude", photoUrl: "/founders/Apoorva.jpeg", portfolioUrl: "/coming-soon" },
 ];
