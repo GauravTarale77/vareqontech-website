af6f8f8b80e2ce4e371994726490ce66165c1f3e
Gaurav Tarale <gauravtarale6@gmail.com>
2026-09-25 12:54:12 +0530

    changes in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://vareqontech.ai/sitemap.xml",
+    sitemap: "https://www.vareqontech.ai/sitemap.xml",
   };
 }
